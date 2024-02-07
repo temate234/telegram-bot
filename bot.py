7779e64d186442a0cbca7f4c6f577980fb791c7b
temate234 <159257045+temate234@users.noreply.github.com>
2024-02-07 21:48:15 +1000

    bot.py

--- bot.py
+++ bot.py
@@ -1,75 +1,64 @@
-#!/usr/bin/env python
-# pylint: disable=unused-argument
-# This program is dedicated to the public domain under the CC0 license.
-
-"""
-Simple Bot to reply to Telegram messages.
-
-First, a few handler functions are defined. Then, those functions are passed to
-the Application and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-
-import logging
 import os
+import telebot
+from telebot import types
 
-from dotenv import load_dotenv
-from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+TELEGRAM_API_TOKEN = '6895670985:AAGjbht0bNbA_HkqBL2GXKyBg2qudquGg3Y'
+bot = telebot.TeleBot(TELEGRAM_API_TOKEN)
 
-# Enable logging
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
-# set higher logging level for httpx to avoid all GET and POST requests being logged
-logging.getLogger("httpx").setLevel(logging.WARNING)
+# Словарь для соответствия русских и английских названий дней недели
+day_mapping = {
+    '/понедельник': '/monday',
+    '/вторник': '/tuesday',
+    '/среда': '/wednesday',
+    '/четверг': '/thursday',
+    '/пятница': '/friday',
+    '/суббота': '/saturday',
+    '/воскресенье': '/sunday'
+}
 
-logger = logging.getLogger(__name__)
+def normalize_command(command):
+    return day_mapping.get(command.lower(), None)
 
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    try:
+        markup = types.ReplyKeyboardMarkup(row_width=2)
+        days = ["/понедельник", "/вторник", "/среда", "/четверг", "/пятница", "/суббота", "/воскресенье"]
+        buttons = [types.KeyboardButton(day) for day in days]
+        markup.add(*buttons)
 
-# Define a few command handlers. These usually take the two arguments update and
-# context.
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /start is issued."""
-    user = update.effective_user
-    await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=True),
-    )
+        bot.reply_to(message, 'Привет! Я бот с расписанием.', reply_markup=markup)
+    except Exception as e:
+        print(f"Error in handle_start: {e}")
 
+@bot.message_handler(func=lambda message: normalize_command(message.text) is not None)
+def handle_schedule(message):
+    try:
+        day = normalize_command(message.text)  # возвращаем английское название дня
+        filename = f"META/{day}.txt"
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /help is issued."""
-    await update.message.reply_text("Help!")
+        if os.path.exists(filename):
+            with open(filename, "r", encoding="utf-8") as file:
+                schedule_text = file.read()
 
+            # Исключаем строку с названием дня из ответа бота
+            schedule_text = schedule_text.replace(f"{day.capitalize()}:", "")
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Echo the user message."""
-    await update.message.reply_text(update.message.text)
+            bot.reply_to(message, f"Расписание:\n\n{schedule_text.strip()}")
+        else:
+            bot.reply_to(message, f"Еблан, дома сиди")
+    except Exception as e:
+        print(f"Error in handle_schedule: {e}")
 
+@bot.message_handler(func=lambda message: True)  # обработка всех остальных сообщений
+def handle_unknown(message):
+    try:
+        bot.reply_to(message, "Неверная команда")
+    except Exception as e:
+        print(f"Error in handle_unknown: {e}")
 
-def main() -> None:
-    """Start the bot."""
-    load_dotenv()
-
-    # Create the Application and pass it your bot's token.
-    application = Application.builder().token(os.environ["TOKEN"]).build()
-
-    # on different commands - answer in Telegram
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help_command))
-
-    # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-
-    # Run the bot until the user presses Ctrl-C
-    application.run_polling(allowed_updates=Update.ALL_TYPES)
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    try:
+        bot.polling(none_stop=True)
+    except Exception as e:
+        print(f"Error in polling: {e}")
